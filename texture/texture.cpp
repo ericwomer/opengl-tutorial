322b0a5ef8cdfff52b38231ee7b81b0db290c0a9
Salamanderrake <salamanderrake@gmail.com>
2014-08-10 15:11:50 -0400

    Added the texture class

--- texture/texture.cpp
+++ texture/texture.cpp
@@ -1,8 +1,25 @@
 #include "texture.h"
 
-Texture::Texture()
-{
+#include <iostream>
 
+Texture::Texture(const std::string& fileName)
+{
+	unsigned int width, height, numComponents;
+
+	SDL_Surface* image;
+	image = IMG_Load(fileName.c_str());
+	if(!image)
+	{
+		std::cerr << "IMG_Load: " << IMG_GetError() << std::endl;
+	}
+
+	width = image->w;
+	height = image->h;
+	numComponents = image->refcount;
+
+
+
+	SDL_FreeSurface(image);
 }
 
 Texture::~Texture()
--- texture/texture.h
+++ texture/texture.h
@@ -1,15 +1,26 @@
 #ifndef TEXTURE_H
 #define TEXTURE_H
 
+#include <GL/glew.h>
+
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL.h>
+
+#include <string>
+
 class Texture
 {
 public:
-	Texture();
+	Texture(const std::string& fileName);
 	~Texture();
+
+	void Bind(unsigned int unit);
 
 private:
 	Texture(Texture& other) {}
 	void operator=(Texture& lhs) {}
+
+	GLuint m_texture;
 };
 
 #endif // TEXTURE_H
